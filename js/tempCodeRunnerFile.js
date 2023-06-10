2dd6f4a66a875d03fcbc1ede973ce857fc88a52d
computerwithalakh <127115267+computerwithalakh@users.noreply.github.com>
2023-06-10 06:26:45 +0530

    file creation

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let btn = document.getElementsByClassName("cf")[0]
+
+// btn.addEventListener("click", function (e) {
+//     let text = document.getElementsByClassName("filename")[0].value
+//     console.log(text);
+// })
