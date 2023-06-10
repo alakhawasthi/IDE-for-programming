2b417695303e56ea2c06fee1ee7791c629e1d0e1
computerwithalakh <127115267+computerwithalakh@users.noreply.github.com>
2023-06-11 00:54:23 +0530

    flask deployment

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def main():
     return render_template('index.html')
+
+@app.route("/start")
+def start():
+    return render_template('start.html')
+
+@app.route("/editor")
+def editor():
+    return render_template('Editor.html')
 
 app.run(debug=True)
