194b2266dc2607676db58ad1aafa0e479f3eb549
computerwithalakh <127115267+computerwithalakh@users.noreply.github.com>
2023-06-11 01:42:29 +0530

    flask app ver 1.0

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route("/start")
+@app.route('/start', methods = ['GET', 'POST'])
 def start():
     return render_template('start.html')
 
-@app.route("/editor")
+@app.route('/editor', methods = ['GET', 'POST'])
 def editor():
+    if request.method == 'POST':
+        file = request.form.get('filename')
+        code = request.form.get('code')
+        fo = open(file + ".js", "w")
     return render_template('Editor.html')
 
 app.run(debug=True)
