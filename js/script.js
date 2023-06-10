2f090eeaf69e1aaacf6614239eefb343f55a0439
computerwithalakh <127115267+computerwithalakh@users.noreply.github.com>
2023-06-10 11:32:58 +0530

    intial commit

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Global Variables
-var run = document.getElementsByClassName("compile")[0]
-
-// Main Logic
-run.addEventListener("click", function (e) {
-    let text = document.getElementsByClassName("code")[0].value
-    
-})
