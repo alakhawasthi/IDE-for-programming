8afc5bf423adfec62bf363e7654b876c588be2c6
computerwithalakh <127115267+computerwithalakh@users.noreply.github.com>
2023-06-10 06:11:38 +0530

    buttion fucntio

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Global Variables
+var run = document.getElementsByClassName("compile")[0]
+
+// Main Logic
+run.addEventListener("click", function (e) {
+    let text = document.getElementsByClassName("code")[0].value
+    
+})
