0a71eee160e9b11a945b12055d1fb68840347375
computerwithalakh <127115267+computerwithalakh@users.noreply.github.com>
2023-06-11 09:03:29 +0530

    v-1.2

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ def main():
 
 @app.route('/start', methods = ['GET', 'POST'])
 def start():
+    if request.method == 'POST':
+        file = request.form.get('filename')
+        filefullname = file + ".js"
+        fo = open(filefullname, "w")
     return render_template('start.html')
 
 @app.route('/editor', methods = ['GET', 'POST'])
 def editor():
     if request.method == 'POST':
         file = request.form.get('filename')
+        f = open(file, "w")
         code = request.form.get('code')
-        fo = open(file + ".js", "w")
+        f.write(code)
     return render_template('Editor.html')
 
 app.run(debug=True)
